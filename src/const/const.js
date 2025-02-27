a50059d45d8cd3ded488bb28765ad9e3547148b2
NehuenRiquelme <88058032+NehuenRiquelme@users.noreply.github.com>
2025-02-27 11:54:24 -0300

    feat(const.js) agrega información sobre estudios y cursos realizados

---
+++ src/const/const.js
@@ -0,0 +1,23 @@
+export const studies = [
+    {
+      title: "Técnico en programacion",
+      corporation: "EPET Nº 20 (Neuquen Capital) ",
+      date: "2/2015 – 12/2022",
+    },
+    {
+      title: "Desarrollador web",
+      corporation: "CoderHouse - Remoto",
+      date: "01/2024 – 03/2024",
+    },
+    {
+      title: "Curso javaScript ",
+      corporation: "CoderHouse - Remoto",
+      date: "04/2024 – 06/2024",
+    },
+    {
+      title: "Curso React Js ",
+      corporation: "CoderHouse - Remoto",
+      date: "06/2024 – 08/2024",
+    },
+
+  ];
