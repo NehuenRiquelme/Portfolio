b39f0a07ca62f610abac977c414837681a4ea2d3
NehuenRiquelme <88058032+NehuenRiquelme@users.noreply.github.com>
2025-03-03 13:53:05 -0300

    fix(const) correccion de las rutas de los archivos

--- src/const/const.js
+++ src/const/const.js
@@ -1,105 +1,25 @@
-export const studies = [
-    {
-      title: "Técnico en programacion",
-      corporation: "EPET Nº 20 (Neuquen Capital) ",
-      date: "2/2015 – 12/2022",
-    },
-    {
-      title: "Desarrollador web",
-      corporation: "CoderHouse - Remoto",
-      date: "01/2024 – 03/2024",
-    },
-    {
-      title: "Curso javaScript ",
-      corporation: "CoderHouse - Remoto",
-      date: "04/2024 – 06/2024",
-    },
-    {
-      title: "Curso React Js ",
-      corporation: "CoderHouse - Remoto",
-      date: "06/2024 – 08/2024",
-    },
-
-  ];
-  export const IconKeys = {
-    html: "html",
-    css: "css",
-    javaScript: "javaScript",
-    react: "react",
-    astro: "astro",
-    sass:"sass",
-    bootstrap: "bootstrap",
-    git: "git",
-    github: "github",
-    vite: "vite",
-    firebase: "firebase",
-  };
-export const skills = [
-    {
-        title: "Html",
-        icon: IconKeys.html,
-    },
-    {
-        title: "Css",
-        icon: IconKeys.css,
-    },
-    {
-        title: "JavaScript",
-        icon: IconKeys.javaScript,
-    },
-    {
-        title: "Astro",
-        icon: IconKeys.astro,
-    },
-    {
-        title: "Sass",
-        icon: IconKeys.sass,
-    },
-    {
-        title: "Bootstrap",
-        icon: IconKeys.bootstrap,
-    },
-    {
-        title: "React Js",
-        icon: IconKeys.react,
-    },
-    {
-        title: "Git",
-        icon: IconKeys.git,
-    },
-    {
-        title: "Github",
-        icon: IconKeys.github,
-    },
-    {
-        title: "Vite",
-        icon: IconKeys.vite,
-    },
-    {
-        title: "Firebase",
-        icon: IconKeys.firebase,
-    },
-];
 export const projects = [
-    {
-      img: "../public/img/pasonInterfaz.webp",
-      title: "Pason",
-      link: "https://nehuenriquelme.github.io/pagina-pason/",
-      description: `interfaz visual para un informe de la empresa pason `,
-      technologies: [
-        {
-          icon: IconKeys.html,
-        },
-        {
-          icon: IconKeys.css,
-        },
-        {
-          icon: IconKeys.javaScript,  
-        },
-      ],
-    },{ img : "./public/img/e-commerce.webp",
+  {
+    img: "../public/img/pasonInterfaz.webp",
+    title: "Pason",
+    link: "https://nehuenriquelme.github.io/pagina-pason/",
+    description: `interfaz visual para un informe de la empresa pason `,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+    ],
+  },
+  {
+    img: "../public/img/e-commerce.webp",
     title: "E-commerce",
-    link:"https://e-commerce-nr.vercel.app/",
+    link: "https://e-commerce-nr.vercel.app/",
     description: `E-commerce de venta de productos de tecnologia`,
     technologies: [
       {
@@ -109,7 +29,7 @@ export const projects = [
         icon: IconKeys.css,
       },
       {
-        icon: IconKeys.javaScript,  
+        icon: IconKeys.javaScript,
       },
       {
         icon: IconKeys.react,
@@ -118,30 +38,31 @@ export const projects = [
         icon: IconKeys.firebase,
       },
     ],
-
-    },{
-      img: "./public/img/Login.webp",
-      title: "Formulario de cotizacion",
-      link: "formulario-cotizacion.vercel.app",
-      description: `Formulario de cotizacion de productos`,
-      technologies: [
-        {
-          icon: IconKeys.html,
-        },
-        {
-          icon: IconKeys.css,
-        },
-        {
-          icon: IconKeys.javaScript,  
-        },
-        {
-          icon: IconKeys.react,
-        },
-        {
-          icon: IconKeys.firebase,
-        },{
-          icon: IconKeys.bootstrap,
-        }
-      ],
-    }];
-  
+  },
+  {
+    img: "../public/img/Login.webp", // Asegúrate de que esta ruta sea correcta
+    title: "Formulario de cotizacion",
+    link: "https://formulario-cotizacion.vercel.app",
+    description: `Formulario de cotizacion de productos`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.firebase,
+      },
+      {
+        icon: IconKeys.bootstrap,
+      },
+    ],
+  },
+];
